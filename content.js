84557f7e35659c0f4f1fb887cb99b54b6846055d
Raleigh Littles III <raleighlittles@gmail.com>
2021-01-03 15:33:56 -0800

    Now can extract all properties from IG page
    Upload date, caption, author, and image url are now logged. Now need to just figure out how to save the image file

--- content.js
+++ content.js
@@ -2,7 +2,18 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "clicked_browser_action" ) {
-        console.log("Raleigh: hello world!");
+          const imageUrl = document.querySelector("meta[property='og:image']").getAttribute('content');
+
+          const jsonLd = JSON.parse(document.querySelector('script[type="application/ld+json"]').innerText);
+
+          const author = jsonLd.author.alternateName;
+          const caption = jsonLd.caption;
+          const uploadDate = jsonLd.uploadDate;
+
+          console.log("Image URL: " + imageUrl);
+          console.log("Uploaded date: " + uploadDate);
+          console.log("Author: " + author);
+          console.log("Caption: " + caption);
       }
     }
   );
